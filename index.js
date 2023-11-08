6ad3958df981b120370f2a110bd2ba0b1ddf162e
Sergeyy321 <125265794+Sergeyy321@users.noreply.github.com>
2023-11-08 22:48:13 +0100

    commit three

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// import yargs from "yargs"
+
 import * as contacts from './contacts.js'
 
 import yargs from 'yargs'
@@ -34,14 +34,4 @@ const { argv } = yargs(process.argv.slice(2));
 invokeAction(argv);
 
 
-// # Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
-// node index.js --action="list"
 
-// # Отримуємо контакт по id і виводимо у консоль об'єкт контакту або null, якщо контакту з таким id не існує.
-// node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
-
-// # Додаємо контакт та виводимо в консоль об'єкт новоствореного контакту
-// node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
-
-// # Видаляємо контакт та виводимо в консоль об'єкт видаленого контакту або null, якщо контакту з таким id не існує.
-// node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH
